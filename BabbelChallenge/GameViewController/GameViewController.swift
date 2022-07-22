7dae8f1373a9c6b8e248d55aefc020026e00a8d3
Onur Avsar <onur.avsar@foreks.com>
2022-07-22 14:50:43 +0300

    Implemented game ui

--- BabbelChallenge/GameViewController/GameViewController.swift
+++ BabbelChallenge/GameViewController/GameViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ViewController: BaseViewController {
+class GameViewController: BaseViewController {
+    
+    @IBOutlet weak var correctAttemptsAmountLabel: UILabel!
+    @IBOutlet weak var wrongAttemptsAmountLabel: UILabel!
+    @IBOutlet weak var spanishWordLabel: UILabel!
+    @IBOutlet weak var englishWordLabel: UILabel!
     
     var viewModel: GameViewModelProtocol?
 
+    //MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //MARK: - Button Actions
 
+    @IBAction func correctButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func wrongButtonTapped(_ sender: Any) {
+        
+    }
 }
