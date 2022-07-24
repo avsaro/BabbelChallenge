ab509678adab75447145b550aa6b42f03b51d4b9
Onur Avsar <onur.avsar@foreks.com>
2022-07-24 19:29:50 +0300

    Added ui tests

--- BabbelChallengeUITests/BabbelChallengeUITests.swift
+++ BabbelChallengeUITests/BabbelChallengeUITests.swift
@@ -8,34 +8,39 @@
 import XCTest
 
 class BabbelChallengeUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRoundTimer() throws {
+        XCTAssertEqual(app.staticTexts["wrongAttempts"].label, "0")
+        sleep(5)
+        XCTAssertEqual(app.staticTexts["wrongAttempts"].label, "1")
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testCorrectTap() throws {
+        app.buttons["Correct"].tap()
+        let wrongAttempts = Int(app.staticTexts["wrongAttempts"].label) ?? 0
+        let correctAttempts = Int(app.staticTexts["correctAttempts"].label) ?? 0
+        let totalAttempts = wrongAttempts + correctAttempts
+        XCTAssertEqual(totalAttempts, 1)
     }
+    
+    func testWrongTap() throws {
+        app.buttons["Wrong"].tap()
+        let wrongAttempts = Int(app.staticTexts["wrongAttempts"].label) ?? 0
+        let correctAttempts = Int(app.staticTexts["correctAttempts"].label) ?? 0
+        let totalAttempts = wrongAttempts + correctAttempts
+        XCTAssertEqual(totalAttempts, 1)
+    }
+    
 }
